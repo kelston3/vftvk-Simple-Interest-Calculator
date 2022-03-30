d5f2b993689067136d7bbfc6d16f9a1bd618d46b
kelston3 <kennyelston33@ymail.com>
2022-03-30 14:58:11 -0500

    Finish Change
    Adjustments to calculator for review

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+  //  p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate/100;
+
+    document.getElementById("result").innerHTML = "If you deposit "+principle 
 }
-        
+
+
+function updateRate() {
+
+  var rateeval = document.getElementById("rate").value;
+//  var rateeval =7;
+  document.getElementById("rate_val").innerText = rateeval;
+
+}
